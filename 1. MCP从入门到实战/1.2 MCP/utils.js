34bd77ea85bc4f1aec62888383778545bb2866e1
傅朝俊 <fuchaojun@szkingdom.com>
2026-03-19 10:44:05 +0800

    fix：1.2 MCP

--- 1. MCP从入门到实战/1.2 MCP/utils.js
+++ 1. MCP从入门到实战/1.2 MCP/utils.js
@@ -31,30 +31,27 @@ export const tools = {
 export default {
   initialize() {
     return {
-      "jsonrpc": "2.0",
-      "id": 1,
-      "result": {
-        "protocolVersion": "2024-11-05",
-        "capabilities": { // 服务端有哪些功能
-          "logging": {},
-          "prompts": {
-            "listChanged": true,
-          },
-          "resources": {
-            "subscribe": true,
-            "listChanged": true,
-          },
-          "tools": {
-            "listChanged": true,
-          },
+      protocolVersion: '2024-11-05',
+      capabilities: {
+        logging: {},
+        prompts: {
+          listChanged: true,
         },
-        "serverInfo": { // 服务端信息
-          "name": "ExampleServer",
-          "title": "Example Server Display Name",
-          "version": "1.0.0",
+        resources: {
+          subscribe: true,
+          listChanged: true,
         },
-        "instructions": "Optional instructions for the client",
+        tools: {
+          listChanged: true,
+        },
       },
+      serverInfo: {
+        // 服务端信息
+        name: 'ExampleServer',
+        title: 'Example Server Display Name',
+        version: '1.0.0',
+      },
+      instructions: 'Optional instructions for the client',
     };
   },
   "tools/list": () => {
