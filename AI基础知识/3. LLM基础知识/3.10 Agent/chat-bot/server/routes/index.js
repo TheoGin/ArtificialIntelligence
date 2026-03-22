2d80af299858741cb5b78fe04b570f45e2c7cc6f
傅朝俊 <fuchaojun@szkingdom.com>
2026-03-22 15:31:13 +0800

    feat: 3.10 Agent

--- AI基础知识/3. LLM基础知识/3.10 Agent/chat-bot/server/routes/index.js
+++ AI基础知识/3. LLM基础知识/3.10 Agent/chat-bot/server/routes/index.js
@@ -2,31 +2,40 @@ const express = require("express");
 const router = express.Router();
 
 // 注意，需要是一个post请求
-router.post("/ask",async (req, res)=>{
-  // 拿到用户的问题
-  const question = req.body.question || "";
-
+// router.route("/ask", async (request, response) => {
+router.post("/ask", async (request, response) => {
+  const question = request.body.question || "";
   // 接下来需要将用户问题放入到提示词模板
   const prompt = `
-    你是一个中文智能助手，请使用中文回答用户的问题。
-    问题：${question}
-  `;
+   你是一个中文智能助手，请使用中文回答用户的问题。
+   问题：${ question }
+   `;
 
-  const response = await fetch("http://localhost:11434/api/generate",{
+  /*
+   curl http://localhost:11434/api/generate -d '{
+   "model": "deepseek-r1:1.5b",
+   "prompt": "你是谁？"
+   }'
+   *  */
+  const respHead = await fetch("http://localhost:11434/api/generate", {
     method: "POST",
-    headers: {"Content-Type": "application/json"},
+    headers: { "content-type": "application/json" },
     body: JSON.stringify({
       model: "qwen3.5:0.8b",
+      // 一般不会直接把用户的问题作为提示词
+      // prompt: question,
       prompt,
-      stream: false // 是否开启流式，暂时先关闭
-    })
-  })
+      stream: false, // 是否开启流式，暂时先关闭
+    }),
+  });
 
-  const result = await response.json();
+  const result = await respHead.json();
+
   // 向客户端返回大模型输出的结果
-  res.json({
-    answer: result.response
-  })
-})
+  response.json({
+    // answer: result,
+    answer: result.response,
+  });
+});
 
 module.exports = router;
